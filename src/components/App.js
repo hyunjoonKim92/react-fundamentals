1e74f5d627bf6b5891636325d2b47130fe7e0040
hyunjoonKim92 <hyunjoon1992@naver.com>
2022-08-11 23:48:30 +0900

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: ''
+        };
+    }
+
+    render() {
+        return (
+            <h1>Hello!!</h1>
+        );
+    }
+}
+
+export default App;
