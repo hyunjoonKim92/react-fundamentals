f3bfc726f1c7e79d4e81ee97a56dc2b33344c878
hyunjoonKim92 <hyunjoon1992@naver.com>
2022-08-17 18:33:30 +0900

    -- 220817 커밋 --- 선택

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ContactInfo from "./ContactInfo";
+import ContactDetails from "./ContactDetails";
 
 export default class Contact extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            selectedKey: -1,
             keyword: '',
             contactData: [{
                name: 'Abet',
@@ -22,12 +24,21 @@ export default class Contact extends React.Component {
         };
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
     handleChange(e) {
         this.setState({
             keyword: e.target.value,
         });
+    }
+
+    handleClick(key) {
+        this.setState({
+            selectedKey: key,
+        });
+
+        console.log(key, ' is selected');
     }
 
     render() {
@@ -38,7 +49,7 @@ export default class Contact extends React.Component {
             });
 
             return data.map((contact, i) => {
-                return (<ContactInfo contact={contact} key={i} />);
+                return (<ContactInfo contact={contact} key={i} onClick={() => this.handleClick(i)} />);
             });
         };
 
@@ -47,6 +58,10 @@ export default class Contact extends React.Component {
                 <h1>Contacts</h1>
                 <input name='keyword' placeholder="Search" value={this.state.keyword} onChange={this.handleChange} />
                 <div>{mapToComponents(this.state.contactData)}</div>
+                <ContactDetails 
+                    isSelected={this.state.selectedKey != -1} 
+                    contact={this.state.contactData[this.state.selectedKey]} 
+                />
             </div>
         );
     }
