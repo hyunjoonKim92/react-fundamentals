9fbe162d83b1d53f1b9bf6b0db2f54501d4be187
hyunjoonKim92 <hyunjoon1992@naver.com>
2022-08-17 19:29:16 +0900

    -- 220817 커밋 --- 글 추가 기능 구현

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ContactInfo from "./ContactInfo";
 import ContactDetails from "./ContactDetails";
+import ContactCreate from "./ContactCreate";
+import update from 'react-addons-update';
 
 export default class Contact extends React.Component {
     constructor(props) {
@@ -25,6 +27,10 @@ export default class Contact extends React.Component {
 
         this.handleChange = this.handleChange.bind(this);
         this.handleClick = this.handleClick.bind(this);
+
+        this.handleCreate = this.handleCreate.bind(this);
+        this.handleRemove = this.handleRemove.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
     }
 
     handleChange(e) {
@@ -41,6 +47,37 @@ export default class Contact extends React.Component {
         console.log(key, ' is selected');
     }
 
+    handleCreate(contact) {
+        this.setState({
+            contactData: update(
+                this.state.contactData, 
+                {$push: [contact]}
+            )
+        });
+    }
+
+    handleRemove() {
+        this.setState({
+            contactData: update(
+                this.state.contactData, 
+                {$splice: [[this.state.selectedKey, 1]]}
+            ),
+            selectedKey: -1
+        });
+    }
+
+    handleEdit(name, phone) {
+        this.setState({
+            contactData: update(
+                this.state.contactData,
+                {[this.state.selectedKey]: {
+                    name: {$set: name},
+                    phone: {$set: phone}
+                }}
+            )
+        });
+    }
+
     render() {
         const mapToComponents = (data) => {
             data.sort();
@@ -49,18 +86,32 @@ export default class Contact extends React.Component {
             });
 
             return data.map((contact, i) => {
-                return (<ContactInfo contact={contact} key={i} onClick={() => this.handleClick(i)} />);
+                return (
+                    <ContactInfo 
+                        contact={contact} 
+                        key={i}
+                        onClick={() => this.handleClick(i)} 
+                    />
+                );
             });
         };
 
         return (
             <div>
                 <h1>Contacts</h1>
-                <input name='keyword' placeholder="Search" value={this.state.keyword} onChange={this.handleChange} />
+                <input 
+                    name="keyword" 
+                    placeholder="Search" 
+                    value={this.state.keyword} 
+                    onChange={this.handleChange} 
+                />
                 <div>{mapToComponents(this.state.contactData)}</div>
                 <ContactDetails 
                     isSelected={this.state.selectedKey != -1} 
                     contact={this.state.contactData[this.state.selectedKey]} 
+                />
+                <ContactCreate 
+                    onCreate={this.handleCreate} 
                 />
             </div>
         );
--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -3,7 +3,11 @@ import React from "react";
 export default class ContactInfo extends React.Component {
     render() {
         return (
-            <div onClick={this.props.onClick}>{this.props.contact.name}</div>
+            <div 
+                onClick={this.props.onClick}
+            >
+                {this.props.contact.name}
+            </div>
         );
     }
 }
